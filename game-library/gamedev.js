e85f4a7c67c990edddc94ff23a16ba2c9af96054
Adam <atmostadam@gmail.com>
2023-10-05 18:04:56 -0400

    init

---
+++ game-library/gamedev.js
@@ -0,0 +1,43 @@
+import { GameContext } from "./context/GameContext.js";
+
+var gameLoop = null;
+
+export function registerGameLoop(loop) { gameLoop = loop; }
+
+window.addEventListener("load", function () {
+    var lastTime = performance.now();
+    const minMsPerFrame = 1000 / 60;
+    const canvas = document.getElementById("game-canvas");
+    const canvasContext = canvas.getContext("2d");
+    var tick = 0;
+
+    new GameContext(canvas, canvasContext);
+
+    function animate() {
+        try {
+            const time = performance.now();
+            const sixtyFps = (time - lastTime) > minMsPerFrame;
+
+            if (sixtyFps && gameLoop) {
+                canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+                gameLoop.update(++tick);
+                gameLoop.draw();
+            }
+
+            window.requestAnimationFrame(animate);
+
+            if (!gameLoop) {
+                console.log("[WARN] gameLoop has not yet been registered. Game implementation has not called registerGameLoop(loop) function.");
+            }
+
+            if (sixtyFps) {
+                lastTime = time;
+            }
+        } catch (e) {
+            canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+            console.error("APPLICATION HAS CRASHED!", e, this);
+            throw e;
+        }
+    }
+    animate();
+});
