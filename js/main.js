d207e1337b410b8a81a985eb18d4a415c201e0a0
Adam <atmostadam@gmail.com>
2023-10-13 22:04:05 -0400

    Add for tomorrow training

---
+++ js/main.js
@@ -0,0 +1,34 @@
+import { GameContext } from "./common/context/GameContext.js";
+
+window.addEventListener("load", function () {
+    var lastTime = performance.now();
+    const minMsPerFrame = 1000 / 60;
+    const canvas = document.getElementById("game-canvas");
+    const canvasContext = canvas.getContext("2d");
+    new GameContext(canvas, canvasContext);
+    var tick = 0;
+
+    function animate() {
+        try {
+            const time = performance.now();
+            const sixtyFps = (time - lastTime) > minMsPerFrame;
+
+            if (sixtyFps) {
+                canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+                GameContext.get("GameLoop").update(++tick);
+                GameContext.get("GameLoop").draw();
+            }
+
+            window.requestAnimationFrame(animate);
+
+            if (sixtyFps) {
+                lastTime = time;
+            }
+        } catch (e) {
+            canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+            console.error("[FATAL] APPLICATION HAS CRASHED!", e, this);
+            throw e;
+        }
+    }
+    animate();
+});
